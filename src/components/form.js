846d0bd50fa1c73a564b21349245a139346c3e0f
AnandPawanipagar <85126312+AnandPawanipagar@users.noreply.github.com>
2021-09-21 19:36:54 +0530

    using grid

---
+++ src/components/form.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+export default function Form() {
+  return (
+    <>
+      <Grid>
+        <Grid>one</Grid>
+        <Grid>two</Grid>
+        <Grid>three</Grid>
+      </Grid>
+    </>
+  );
+}
