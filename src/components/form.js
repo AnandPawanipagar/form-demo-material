914276a807d0908ca69e9141a7ffbf29e8aebeb0
AnandPawanipagar <85126312+AnandPawanipagar@users.noreply.github.com>
2021-09-22 13:18:07 +0530

    made ui for form using material  with 4 text fields

--- src/components/form.js
+++ src/components/form.js
@@ -1,13 +1,61 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
-
+import { Grid } from "@material-ui/core/";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/styles";
+import TextField from "@material-ui/core/TextField";
+// import Box from "@material-ui/core/Box";
+const useStyles = makeStyles({
+  field: {
+    margin: 10,
+    
+  },
+});
 export default function Form() {
+  const classes = useStyles();
   return (
     <>
-      <Grid>
-        <Grid>one</Grid>
-        <Grid>two</Grid>
-        <Grid>three</Grid>
+      <Grid container >
+        <Grid item xs={12}>
+          <Typography variant="h4" color="initial" className={classes.field}>
+            Registration Form
+          </Typography>
+        </Grid>
+
+        <Grid item sm={6}>
+          <TextField
+            className={classes.field}
+            label="Name"
+            variant="outlined"
+            color="secondary"
+            fullWidth
+            required
+          ></TextField>
+          <TextField
+            className={classes.field}
+            label="Contact number"
+            variant="outlined"
+            color="secondary"
+            fullWidth
+            required
+          ></TextField>
+        </Grid>
+        <Grid item sm={6}>
+          <TextField
+            className={classes.field}
+            label="Address"
+            variant="outlined"
+            color="secondary"
+            fullWidth
+          ></TextField>
+          <TextField
+            className={classes.field}
+            label="Email"
+            variant="outlined"
+            color="secondary"
+            fullWidth
+            required
+          ></TextField>
+        </Grid>
       </Grid>
     </>
   );
